cd171b89fccb383ea537ea519f5e2655462a8ff3
laKsHyA1331 <120799920+laKsHyA1331@users.noreply.github.com>
2023-12-17 17:00:24 +0530

    Add files via upload

---
+++ Sandbox/src/sandboxApp.cpp
@@ -0,0 +1,16 @@
+#include<hazel.h>
+
+
+class Sandbox : public hazel::Application {
+public:
+	Sandbox() {
+
+	}
+	~Sandbox() {
+
+	}
+};
+
+ hazel::Application* hazel::createApplication() {
+	return new Sandbox(); 
+}
